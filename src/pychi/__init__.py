d0f9f7a2db1e25f06adf4f775d91673d011ba64d
TVoumard <97957751+TVoumard@users.noreply.github.com>
2025-07-15 10:52:55 +0200

    Update __init__.py

--- src/pychi/__init__.py
+++ src/pychi/__init__.py
@@ -4,9 +4,10 @@ Created on Thu Feb 17 12:32:49 2022
 
 @author: voumardt
 """
-__version__ = '0.0.5'
+__version__ = '0.0.17'
 
 from . import light
 from . import materials
 from . import models
 from . import solvers
+from . import utils
